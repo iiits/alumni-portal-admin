e33ac94a2478544aa6d0ae668f7049be737798dd
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2025-07-25 17:25:56 +0530

    [Add] implement GET endpoints for alumni, contacts, dashboard, events, jobs, referrals, and users analytics

---
+++ app/api/admin/jobs-analytics/route.tsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const token = req.cookies.get("token")?.value;
+
+    if (!token) {
+      return NextResponse.json(
+        { message: "Not authorized - No token provided." },
+        { status: 401 },
+      );
+    }
+
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/admin/jobs-analytics`,
+      { headers: { Authorization: `Bearer ${token}` } },
+    );
+
+    return NextResponse.json(response.data, { status: response.status });
+  } catch (error: any) {
+    console.error(
+      "Error fetching user profile:",
+      error.response?.data || error.message,
+    );
+    return NextResponse.json(
+      {
+        message:
+          error.response?.data?.message || "Failed to fetch user profile.",
+      },
+      { status: error.response?.status || 500 },
+    );
+  }
+}
